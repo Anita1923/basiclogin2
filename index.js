be3e57cc101f7fd3bfdcc1557d5b8d722a3f7b6f
Anita1923 <anitagobinathan19@gmail.com>
2023-04-11 14:45:50 +0800

    login

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+let dbUsers = [
+  {
+      username: "anita",
+      password: "192329",
+      name: "Anita",
+      email:"anita@utem.edu.my"
+  },
+  {
+      username: "jeev",
+      password: "230500",
+      name: "Jeev",
+      email:"jeev@utem.edu.my"
+  },
+  {
+      username: "mark",
+      password: "190600",
+      name: "Mark",
+      email:"mark@utem.edu.my"
+  }
+]
+
+function login(username, password) {
+  //let matchUser= dbUsers.find(user => user.username == username)
+  let matchUser= dbUsers.find(x => x.username == username)
+  if(!matchUser) return "User not found!"
+  if(matchUser.password==password){
+      return matchUser
+  } else {
+      return "Invalid password"
+  }
+} 
+
+function register(username, password, name, email) {
+  dbUsers.push({
+      username: username,
+      password: password,
+      name: name,
+      email: email
+  })
+}
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -21,6 +62,13 @@ app.get('/bye', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
+  let result=register(
+    req.body.username,
+    req.body.password,
+    req.body.name,
+    req.body.email
+  )
+  
   res.send('ACCOUNT CREATED!')
 })
 
